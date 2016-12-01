3acaad0e344f7818d7138d0f473c2f6d27d8ccbe
hayaka26 <miz563@gmail.com>
2016-12-01 13:05:49 +0900

    page_theme.phpを変更

--- development/themes/simple_style/page_theme.php
+++ development/themes/simple_style/page_theme.php
@@ -7,19 +7,7 @@ class PageTheme extends \Concrete\Core\Page\Theme\Theme
     {
         //$this->providesAsset('javascript', 'bootstrap/*');
         $this->providesAsset('css', 'bootstrap/*');
-        $this->providesAsset('css', 'blocks/form');
-        $this->providesAsset('css', 'blocks/social_links');
-        $this->providesAsset('css', 'blocks/share_this_page');
-        $this->providesAsset('css', 'blocks/feature');
-        $this->providesAsset('css', 'blocks/testimonial');
-        $this->providesAsset('css', 'blocks/date_navigation');
-        $this->providesAsset('css', 'blocks/topic_list');
-        $this->providesAsset('css', 'blocks/faq');
-        $this->providesAsset('css', 'blocks/tags');
         $this->providesAsset('css', 'core/frontend/*');
-        $this->providesAsset('css', 'blocks/feature/templates/hover_description');
-
-        $this->providesAsset('css', 'blocks/event_list');
 
         $this->requireAsset('css', 'font-awesome');
         $this->requireAsset('javascript', 'jquery');
